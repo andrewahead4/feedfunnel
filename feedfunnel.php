6138e52510f09ff3a7f1e5f3df066f1a7b548497
Andrew Fielden <andrew.fielden@ahead4.biz>
2015-10-11 12:08:34 +0100

    initial details

---
+++ feedfunnel.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Feed Funnel
+* Plugin URI: http://andrew.ahead4.biz/feedfunnel
+* Description: Imports RSS feeds and allows management of them
+* Version: 0.1
+* Author: Andrew Fielden
+* Author URI: http://andrew.ahead4.biz/f
+* License: A "Slug" license name e.g. GPL12
+*/
